e35ad0d7d099f11e0b556109f764434ba42d89f4
soliha10 <toxtamurodovagulshodaa@gmail.com>
2023-12-23 10:56:19 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var userName = prompt("Ismingizni kiriting");
+var userLastname = prompt("Familiyangizni kiriting");
+var userAge = prompt("Yoshingizni kiriting");
+var userEmail = prompt("Emailingizni kiriting");
+var usaMoney = prompt("Summa kiriting ($)");
+var uzbMoney = 12280;
+var moneyResult = Number(usaMoney) * uzbMoney;
+ console.log("Foydalanuvchi ismi: " + userName, "\n", "Familiyasi: " + userLastname, "\n", "Yoshi: " + userAge, "\n", "Emaili: " + userEmail, "\n", "Kiritilgan dollar uzb so'mida " + moneyResult + " bo'ladi");
+
